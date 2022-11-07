e9e320d8c8f2541d193f8240ef2be317c6d9f57b
Mustafa Zamani <zamani.mustafa91@gmail.com>
2022-11-07 15:49:26 +0100

    test

---
+++ php/bsics/calculator/index.php
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset = "UTF-8">
+        <title></title>
+    </head>
+
+    <body>
+        <form action="">
+            <input type="text" name="num1" placeholder="Number 1">
+            <input type="text" name="nam2" placeholder="Number 2">
+            <select name="" id="">
+                <option value="">None</option>
+                <option value="">add</option>
+                <option value="">subtract</option>
+                <option value="">Multiply</option>
+                <option value="">divide</option>
+            </select>
+            <br>
+            <button typ="submit" name="submit" value="submit">Calculate</button>
+        </form>
+
+    </body>
+</html>
